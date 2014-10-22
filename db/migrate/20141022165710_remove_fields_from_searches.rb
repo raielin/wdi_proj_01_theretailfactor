33fde3d6fed90ad78855412c3d7a065af7d9ea26
raielin@gmail.com <raielin@gmail.com>
2014-10-22 13:16:18 -0400

    Dropped database adn modified remove_columns to include datatype. This is required for rake db:rollback to work.

--- db/migrate/20141022165710_remove_fields_from_searches.rb
+++ db/migrate/20141022165710_remove_fields_from_searches.rb
@@ -1,7 +1,6 @@
 class RemoveFieldsFromSearches < ActiveRecord::Migration
   def change
-    remove_column :searches, :name
-    remove_column :searches, :description
-    remove_timestamps :searches
+    remove_column :searches, :name, :text
+    remove_column :searches, :description, :text
   end
 end
