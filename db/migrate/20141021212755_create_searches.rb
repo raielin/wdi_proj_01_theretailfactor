d707205e47b51222d867f32a4cd13284476b862b
raielin@gmail.com <raielin@gmail.com>
2014-10-22 11:36:48 -0400

    Updated the Create Searches migration to have fields

--- db/migrate/20141021212755_create_searches.rb
+++ db/migrate/20141021212755_create_searches.rb
@@ -1,6 +1,10 @@
 class CreateSearches < ActiveRecord::Migration
   def change
     create_table :searches do |t|
+      t.belongs_to :user, index: true
+      t.text :name
+      t.text :description
+      t.timestamps
     end
   end
 end
