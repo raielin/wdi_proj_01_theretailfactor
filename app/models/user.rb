36b26e0696be3d1daef8f5c0128d642210f2ecf9
raielin@gmail.com <raielin@gmail.com>
2014-10-22 00:47:27 -0400

    Overwrite Devise's find_for_database_authentication method in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ActiveRecord::Base
   # This is in addition to a real persisted field like 'username'
   attr_accessor :login
 
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
