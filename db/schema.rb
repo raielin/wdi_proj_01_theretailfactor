5229ac11aaf3efbaa7b7099c80bd1724de8e756f
raielin@gmail.com <raielin@gmail.com>
2014-10-22 10:55:22 -0400

    Ran rake db:migrate. Created new fields in user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021013834) do
+ActiveRecord::Schema.define(version: 20141022021542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "markets", force: true do |t|
+  end
+
+  create_table "prop_criterias", force: true do |t|
+  end
+
+  create_table "properties", force: true do |t|
+  end
+
+  create_table "searches", force: true do |t|
+  end
+
+  create_table "targets", force: true do |t|
+  end
+
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                    default: "", null: false
+    t.string   "encrypted_password",       default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",            default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "first_name",               default: ""
+    t.text     "last_name",                default: ""
+    t.text     "username",                 default: "", null: false
+    t.string   "profile_pic_file_name"
+    t.string   "profile_pic_content_type"
+    t.integer  "profile_pic_file_size"
+    t.datetime "profile_pic_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
