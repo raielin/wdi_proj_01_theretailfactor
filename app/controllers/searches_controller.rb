82d982c32e1974bfb96e68dbca3d19b340967ea3
raielin@gmail.com <raielin@gmail.com>
2014-10-22 11:49:21 -0400

    Creation of new basic searches now work.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,12 +7,23 @@ class SearchesController < ApplicationController
   # Creating a new search
   def create
     # Make a new search and now do something
-
-    redirect_to @search
+    # TODO: Associate the current user with this
+    @search = Search.new(search_params)
+    if @search.save
+      redirect_to @search
+    else
+      render :new
+    end
   end
 
   # Looking at results for a search
   def show
     @search = Search.find(params[:id])
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:name, :description)
   end
 end
