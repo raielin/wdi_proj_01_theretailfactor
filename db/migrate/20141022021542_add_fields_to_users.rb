a78ab515c3c2c45c18260f09ed2896c0b51fb331
raielin@gmail.com <raielin@gmail.com>
2014-10-21 23:59:20 -0400

    Created new migration to add extra fields to User model, including username.

--- db/migrate/20141022021542_add_fields_to_users.rb
+++ db/migrate/20141022021542_add_fields_to_users.rb
@@ -1,4 +1,9 @@
 class AddFieldsToUsers < ActiveRecord::Migration
   def change
+    add_column :users, :first_name, :text, default: ""
+    add_column :users, :last_name, :text, default: ""
+    add_column :users, :username, :text, null: false, default: ""
+    add_index :users, :username, unique: true
+    add_column :users, :picture, :binary, limit: 2.megabytes
   end
 end
