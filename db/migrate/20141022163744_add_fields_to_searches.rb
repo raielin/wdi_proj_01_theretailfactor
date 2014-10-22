075b1ffa2789f9cb6d027ed7839f6ced331cb3aa
raielin@gmail.com <raielin@gmail.com>
2014-10-22 12:49:29 -0400

    Added fields to searches for use with revised concept.

--- db/migrate/20141022163744_add_fields_to_searches.rb
+++ db/migrate/20141022163744_add_fields_to_searches.rb
@@ -1,4 +1,7 @@
 class AddFieldsToSearches < ActiveRecord::Migration
   def change
+    add_column :searches, :city, :text
+    add_column :searches, :state, :text
+    add_column :searches, :zip, :text
   end
 end
