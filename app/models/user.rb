20aab1a7a0cadcf48d1a0238d450c756f634a26f
raielin@gmail.com <raielin@gmail.com>
2014-10-22 11:40:24 -0400

    Split User validation into multiple lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,9 @@ class User < ActiveRecord::Base
     end
   end
 
-  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
+  validates :username,
+    presence: true,
+    length: {maximum: 255},
+    uniqueness: { case_sensitive: false },
+    format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
 end
