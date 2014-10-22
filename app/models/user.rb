f87e2adfa94a06f51f3e285f5f3a0b6b503ed32a
raielin@gmail.com <raielin@gmail.com>
2014-10-22 10:35:15 -0400

    Modified config/initializers/devise.rb and users model to allow users to recover their password or confirm their account using either username or email address. Also added login validation to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   # This is in addition to a real persisted field like 'username'
   attr_accessor :login
 
-  def self.find_for_database_authentication(warden_conditions)
+  def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
       where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
@@ -16,4 +16,6 @@ class User < ActiveRecord::Base
       where(conditions).first
     end
   end
+
+  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
 end
