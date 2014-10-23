ed74099d2fff3facf77ff8d2fe989b84a9120749
raielin@gmail.com <raielin@gmail.com>
2014-10-22 20:59:53 -0400

    Notes to sample api queries.

--- test/api_queries.rb
+++ test/api_queries.rb
@@ -18,9 +18,39 @@ end
 
 
 
-demos = Rubillow::Neighborhood.demographics({ state: "WA", city: "Seattle", neighborhood: "Ballard"})
-if demos.success?
-  puts demos.charts['Median Home Value']
+# demos = Rubillow::Neighborhood.demographics({ state: "WA", city: "Seattle", neighborhood: "Ballard"})
+# if demos.success?
+#   puts demos.charts['Median Home Value']
+# else
+#   puts false
+# end
+
+
+# http://www.zillow.com/howto/api/GetRegionChildren.htm
+
+neighborhoods = Rubillow::Neighborhood.region_children({state:"MA", city: "Boston", childtype: "neighborhood"})
+binding.pry
+if neighborhoods.success?
+  neighborhoods.regions.each do |region|
+    puts region.id
+  end
 else
   puts false
 end
+
+
+=begin
+
+neighborhoods = Rubillow::Neighborhood.region_children({state:"MA", city: "Boston", childtype: "neighborhood"})
+
+neighborhoods.region.id                       => "44269"
+neighborhoods.regions[1].id                   => "54232"
+neighborhoods.regions.count                   => 29
+neighborhoods.regions[0].neighborhood         => ""
+
+=end
+
+# Modify Rubillow file /lib/rubillow/models/region.rb.
+# Try one:
+  # @neighborhood = @parser.xpath('//neighborhood').first.text
+  # @neighborhood = @parser.xpath('//neighborhood').attribute('name').value
