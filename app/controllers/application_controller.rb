7773a4233066d774c0c354c25975549cff5ea447
raielin@gmail.com <raielin@gmail.com>
2014-10-23 00:08:03 -0400

    Added commented-out code for search method to be refined later.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,16 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # TODO: Create method to run search
+  # def query(city, state, zip)
+  #   Rubillow::Neighborhood.region_children({state: state, city: city, childtype: "neighborhood"})
+  #   @neighborhoods = []
+  #   query.regions.each do |region|
+  #     @neighborhoods << region.neighborhood
+  #   end
+  # end
+
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
