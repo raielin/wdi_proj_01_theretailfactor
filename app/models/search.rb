ed2abb15705d29e79ea5aa545cad83f1c5df4b84
raielin@gmail.com <raielin@gmail.com>
2014-10-23 00:15:32 -0400

    Put draft search method in wrong place. Moved from application controller to search model.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,15 @@
 class Search < ActiveRecord::Base
   belongs_to :user
+
+
+  # TODO: Create method to run search
+  # def query(city, state, zip)
+  #   Rubillow::Neighborhood.region_children({state: state, city: city, childtype: "neighborhood"})
+  #   @neighborhoods = []
+  #   query.regions.each do |region|
+  #     @neighborhoods << region.neighborhood
+  #   end
+  # end
+
+
 end
