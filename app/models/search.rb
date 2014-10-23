a6e1946726dfd67c5b239bc1998b8ae7ede6acf7
raielin@gmail.com <raielin@gmail.com>
2014-10-22 23:49:00 -0400

    Deleted commented-out associations no longer needed.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,3 @@
 class Search < ActiveRecord::Base
   belongs_to :user
-  # has_one :propcriteria, dependent: :destroy
-  # has_one :target, dependent: :destroy
-  # has_one :market, dependent: :destroy
-  # has_many :properties, dependent: :destroy
-
 end
