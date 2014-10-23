1079be72dbc52726bd0261f6358c7d02fed8b3f8
raielin@gmail.com <raielin@gmail.com>
2014-10-23 01:40:09 -0400

    Added search method. Need to test and refine.

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,14 +2,15 @@ class Search < ActiveRecord::Base
   belongs_to :user
 
 
-  # TODO: Create method to run search
-  # def query(city, state, zip)
-  #   Rubillow::Neighborhood.region_children({state: state, city: city, childtype: "neighborhood"})
-  #   @neighborhoods = []
-  #   query.regions.each do |region|
-  #     @neighborhoods << region.neighborhood
-  #   end
-  # end
+  # TODO: Create method to run search.
+  # TODO: Add zip as optional field.
+  def self.results(city, state)
+    Rubillow::Neighborhood.region_children({state: state, city: city, childtype: "neighborhood"})
+    @neighborhoods = []
+    query.regions.each do |region|
+      @neighborhoods << region.neighborhood
+    end
+  end
 
 
 end
