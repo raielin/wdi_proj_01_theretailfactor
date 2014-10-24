43cb1048c00aa813a0f93b6cd9c2d0c504848c92
raielin@gmail.com <raielin@gmail.com>
2014-10-24 15:19:51 -0400

    A search has many results

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,17 +1,5 @@
 class Search < ActiveRecord::Base
   belongs_to :user
-
-
-  # TODO: Not sure if this is the right way to set it up - define method in model and call in search controller action?
-  # TODO: Add zip as optional field.
-  # TODO: Validate input
-  # def results
-  #   @results = Rubillow::Neighborhood.region_children({state: @self.state, city: @self.city, childtype: "neighborhood"})
-  #   @neighborhoods = []
-  #   @results.regions.each do |region|
-  #     @neighborhoods << region.neighborhood
-  #   end
-  # end
-
+  has_many :results
 
 end
